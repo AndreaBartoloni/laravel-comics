0171648fb591dcade6dab64a7d7212d95a4a9abd
AndreaBartoloni <andrea.bartoloni00@gmail.com>
2024-01-22 09:36:14 +0100

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,188 @@
-{{-- 
-@php 
-    $comics = include('src/config/data.php');
-@endphp --}}
+
 
 @extends('layout.app')
 
 @section('content')
-    @foreach($comics as $comic)
-
-        <li>{{$comic['title']}}</li>
-
-    @endforeach
+    <div class="container-jumbo" >
+        <img src="{{ asset('img/jumbotron.jpg')}}">
+    </div>
+    <div class="container-content">
+        <div class="container-bg">
+            <p class="background-blue">Current Series</p>
+        </div>
+        
+        <div class="container-bg flex">
+            
+            @foreach($comics as $comic)
+                <div class="card-fumetti">
+                    <img src="{{$comic['thumb']}}">
+                    <h5>{{$comic['series']}}</h5>
+                    {{-- <p>{{$comic['description']}}</p> --}}
+                </div>
+            @endforeach
+                
+            
+        </div>
+        <button class="button">Load More</button>
+        <div class="sfondo-blu-iconbar">
+            <div class="container-iconbar container-bg">
+                <div class="card-icon">
+                    <div class="container-image">
+                        <img src="{{ asset('img/buy-comics-digital-comics.png')}}">
+                    </div>
+                    <div class="container-title">
+                        <p></p>
+                    </div>
+                    
+                </div>
+                <div class="card-icon">
+                    <div class="container-image">
+                        <img src="{{ asset('img/buy-comics-digital-comics.png')}}">
+                    </div>
+                    <div class="container-title">
+                        <p></p>
+                    </div>
+                    
+                </div>
+                <div class="card-icon">
+                    <div class="container-image">
+                        <img src="{{ asset('img/buy-comics-digital-comics.png')}}">
+                    </div>
+                    <div class="container-title">
+                        <p></p>
+                    </div>
+                    
+                </div>
+                <div class="card-icon">
+                    <div class="container-image">
+                        <img src="{{ asset('img/buy-comics-digital-comics.png')}}">
+                    </div>
+                    <div class="container-title">
+                        <p></p>
+                    </div>
+                    
+                </div>
+                <div class="card-icon">
+                    <div class="container-image">
+                        <img src="{{ asset('img/buy-comics-digital-comics.png')}}">
+                    </div>
+                    <div class="container-title">
+                        <p></p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    
 
 @endsection
+
+
+<style>
+/* Sezione Hero */
+.container-jumbo > img{
+    width: 100%;
+    height: 37vh;
+    object-fit: cover;
+    object-position: top;
+}
+/* Content */
+.container-content{
+    min-height:auto;
+    background-color: #1a1a1a;
+    text-align: center;
+    margin-top: -25px;
+   
+}
+.flex{
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+.card-fumetti{
+    
+    width: 15%;
+}
+.card-fumetti > img{
+   width: 100%;
+   height: 150px;
+   object-fit: cover;
+   object-position: top;
+}
+.card-fumetti > h5{
+    margin-top: 10px;
+    text-align: start;
+    color: white;
+    font-size: 15px;
+}
+.background-blue{
+    background-color: #057bf3;
+    width: 18%;
+    padding: 10px 20px ;
+    text-transform: uppercase;
+    text-align: start;
+    margin: 0px 0px 20px 0px;
+    font-weight: 600;
+    z-index: 99;
+    position: relative;
+    
+}
+p{
+    color: white;
+}
+
+.button {
+
+    border: 1px solid transparent;
+    padding: 0.6em 2.4em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: #057bf3;
+    cursor: pointer;
+    transition: border-color 0.25s;
+    text-align: center;
+    margin: 30px 0px 20px 0px ;
+    color: white;
+
+}
+
+/* Icon Bar  */
+
+.sfondo-blu-iconbar{
+    background-color: #057bf3;
+    padding: 10px 0px;
+}
+.container-iconbar{
+    display: flex;
+    align-items: center;
+    text-align: center;
+    padding: 50px 0px;
+}
+.card-icon{
+    width: 20%;
+    display: flex;
+    align-items: center;
+    margin-right: 0px;
+    
+}
+.container-image{
+    width: 30%;
+}
+.container-image > img{
+    width: 80%;
+    text-align: start;
+}
+.container-title{
+    width: 70%;
+}
+.container-title > p{
+    text-transform: uppercase;
+    font-size: 13px;
+    text-align: start;
+    margin-left: 5px;
+}
+
+</style>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,52 @@
+
+<link rel="stylesheet" href="{{ asset('/style.css') }}">
+
 <header>
 
-    <a href="{{ route('home') }}">Home</a>
+    <div class="container-testata container-bg">
+        <div class="container-img">
+          <img src="{{ asset('img/dc-logo.png') }}">
+        </div>
+        <div class="container-navbar">
+            <a href="{{ route('home') }}">Home</a>
+            <a href="{{ route('home') }}">Home</a>
+            <a href="{{ route('home') }}">Home</a>
+            <a href="{{ route('home') }}">Home</a>
+            <a href="{{ route('home') }}">Home</a>
+            <a href="{{ route('home') }}">Home</a>
+        </div>
+    </div>
 
 </header>
+
+<style>
+
+
+.container-testata{
+  display: flex;
+  align-items: center;
+
+}
+header{
+
+  background-color:white;
+  padding: 20px 0px;
+}
+a{
+  color: black;
+  margin-right: 30px;
+  text-transform: uppercase;
+  font-size: 11px;
+  font-weight: 600;
+}
+.container-img{
+  width: 100%;
+} 
+.container-img > img{
+  width: 15%;
+} 
+.container-navbar{
+  width: 75%;
+}
+
+</style>
